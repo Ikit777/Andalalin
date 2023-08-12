4ac078f3fb20e16983387ab7a687f5c5027cd9f4
ikit777 <faisalmtp24@gmail.com>
2023-08-13 01:53:31 +0800

    E commit

--- main.go
+++ main.go
@@ -1,18 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"strings"
-	"time"
 
 	"github.com/Ikit777/E-Andalalin/controllers"
 	"github.com/Ikit777/E-Andalalin/initializers"
-	"github.com/Ikit777/E-Andalalin/models"
 	"github.com/Ikit777/E-Andalalin/routes"
-	"github.com/Ikit777/E-Andalalin/utils"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
@@ -54,46 +48,6 @@ func main() {
 	if err != nil {
 		log.Fatal("🚀 Could not load environment variables", err)
 	}
-
-	initializers.DB.Exec("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\"")
-
-	initializers.DB.Migrator().DropTable(&models.User{})
-	initializers.DB.Migrator().DropTable(&models.Andalalin{})
-	initializers.DB.Migrator().DropTable(&models.Survey{})
-	initializers.DB.Migrator().DropTable(&models.TiketLevel1{})
-	initializers.DB.Migrator().DropTable(&models.TiketLevel2{})
-
-	initializers.DB.AutoMigrate(&models.User{})
-	initializers.DB.AutoMigrate(&models.Andalalin{})
-	initializers.DB.AutoMigrate(&models.Survey{})
-	initializers.DB.AutoMigrate(&models.TiketLevel1{})
-	initializers.DB.AutoMigrate(&models.TiketLevel2{})
-
-	loc, _ := time.LoadLocation("Asia/Hong_Kong")
-	now := time.Now().In(loc).Format("02-01-2006")
-	hashedPassword, err := utils.HashPassword("superadmin")
-	if err != nil {
-		return
-	}
-
-	filePath := "assets/default.png"
-	fileData, err := os.ReadFile(filePath)
-	if err != nil {
-		log.Fatal("Error reading the file:", err)
-	}
-
-	initializers.DB.Create(&models.User{
-		Name:      "super admin",
-		Email:     strings.ToLower("superadmin@gmail.com"),
-		Password:  hashedPassword,
-		Role:      "Super Admin",
-		Photo:     fileData,
-		Verified:  true,
-		CreatedAt: now,
-		UpdatedAt: now,
-	})
-
-	fmt.Println("Migration complete")
 
 	corsConfig := cors.DefaultConfig()
 	corsConfig.AllowOrigins = []string{"http://0.0.0.0:8080", config.ClientOrigin}
