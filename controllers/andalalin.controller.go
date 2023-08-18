2de2998c34852d8ef5692aacc3195b3edd48d882
ikit777 <faisalmtp24@gmail.com>
2023-08-18 13:55:03 +0800

    I commit

--- controllers/andalalin.controller.go
+++ controllers/andalalin.controller.go
@@ -32,7 +32,7 @@ func NewAndalalinController(DB *gorm.DB) AndalalinController {
 
 // const path = "C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf"
 
-const path = "/usr/local/bin/wkhtmltopdf"
+const path = "/app/wkhtmltopdf"
 
 func (ac *AndalalinController) Pengajuan(ctx *gin.Context) {
 	var payload *models.DataAndalalin
