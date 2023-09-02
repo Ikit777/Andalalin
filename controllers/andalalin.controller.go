e0c59d90c3988d9c1842f631b182c234ea4132b9
ikit777 <faisalmtp24@gmail.com>
2023-09-02 21:10:54 +0800

    init

--- controllers/andalalin.controller.go
+++ controllers/andalalin.controller.go
@@ -516,7 +516,7 @@ func (ac *AndalalinController) GetPermohonanPersetujuan(ctx *gin.Context) {
 
 	var andalalin []models.Andalalin
 
-	results := ac.DB.Find(&andalalin, "status_andalalin = ? AND persetujuan_dokumen IS NULL", "Laporan BAP")
+	results := ac.DB.Order("tanggal_andalalin").Find(&andalalin, "persetujuan_dokumen IS NULL")
 
 	if results.Error != nil {
 		ctx.JSON(http.StatusBadGateway, gin.H{"status": "error", "message": results.Error})
