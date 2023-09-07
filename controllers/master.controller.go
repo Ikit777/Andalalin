0247fc30b5cb9b3c8484c4c67b5d905f6aa5fcd0
ikit777 <faisalmtp24@gmail.com>
2023-09-07 11:57:08 +0800

    I commit

--- controllers/master.controller.go
+++ controllers/master.controller.go
@@ -79,5 +79,13 @@ func (dm *DataMasterControler) TambahLokasi(ctx *gin.Context) {
 
 	var master models.DataMaster
 
-	dm.DB.Model(&master).Association("LokasiPengambilan").Append(lokasi)
+	results := dm.DB.First(&master.LokasiPengambilan, payload.Lokasi)
+	if results.Error == nil {
+		ctx.JSON(http.StatusConflict, gin.H{"status": "fail", "message": "Usulan sudah ada"})
+		return
+	} else {
+		dm.DB.Model(&master).Association("LokasiPengambilan").Append(&lokasi)
+
+		ctx.JSON(http.StatusOK, gin.H{"status": "success"})
+	}
 }
