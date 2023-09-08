05d68a8c668209644db9f3494bbac05ee024251c
ikit777 <faisalmtp24@gmail.com>
2023-09-08 21:19:53 +0800

    I commit

--- controllers/master.controller.go
+++ controllers/master.controller.go
@@ -410,22 +410,18 @@ func (dm *DataMasterControler) TambahJenisRencanaPembangunan(ctx *gin.Context) {
 		return
 	}
 
-	var rencana_pembangunan models.Rencana
-
 	for i, kategori_jenis := range master.RencanaPembangunan {
 		if kategori_jenis.Kategori == kategori {
 
-			rencana_pembangunan = kategori_jenis
-
-			exist := contains(rencana_pembangunan.JenisRencana, rencana)
+			exist := contains(kategori_jenis.JenisRencana, rencana)
 
 			if exist {
 				ctx.JSON(http.StatusConflict, gin.H{"status": "fail", "message": "Data sudah ada"})
 				return
+			} else {
+				kategori_jenis.JenisRencana = append(kategori_jenis.JenisRencana, rencana)
+				master.RencanaPembangunan[i] = kategori_jenis
 			}
-
-			rencana_pembangunan.JenisRencana = append(rencana_pembangunan.JenisRencana, rencana)
-			master.RencanaPembangunan[i] = rencana_pembangunan
 			break
 		} else {
 			jenis := models.Rencana{
