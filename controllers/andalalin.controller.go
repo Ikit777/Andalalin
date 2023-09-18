cfd0fdae0c1e3933365b72dcd532c6b257465ded
ikit777 <faisalmtp24@gmail.com>
2023-09-18 14:01:36 +0800

    commit

--- controllers/andalalin.controller.go
+++ controllers/andalalin.controller.go
@@ -263,7 +263,7 @@ func (ac *AndalalinController) PengajuanPerlalin(ctx *gin.Context) {
 	loc, _ := time.LoadLocation("Asia/Singapore")
 	nowTime := time.Now().In(loc)
 
-	kode := "andalalin/" + utils.Generate(6)
+	kode := "perlalin/" + utils.Generate(6)
 	tanggal := nowTime.Format("02") + " " + utils.Bulan(nowTime.Month()) + " " + nowTime.Format("2006")
 
 	t, err := template.ParseFiles("templates/tandaterimaPerlalin.html")
