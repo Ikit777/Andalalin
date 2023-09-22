b5c088285c31f78ceffa81e63566ef48f24e5b03
ikit777 <faisalmtp24@gmail.com>
2023-09-22 20:49:20 +0800

    commit

--- models/andalalin.model.go
+++ models/andalalin.model.go
@@ -454,5 +454,5 @@ type SurveiKepuasan struct {
 type SurveiKepuasanInput struct {
 	IdAndalalin uuid.UUID `json:"id_andalalin" binding:"required"`
 	KritikSaran string    `json:"saran" binding:"required"`
-	DataSurvei  []string  `gorm:"serializer:json"`
+	DataSurvei  []string  `json:"data" binding:"required"`
 }
