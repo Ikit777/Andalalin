98742170393f6caf9aee4773480e33658d39cb05
ikit777 <faisalmtp24@gmail.com>
2023-09-25 19:53:02 +0800

    commit

--- controllers/andalalin.controller.go
+++ controllers/andalalin.controller.go
@@ -2850,25 +2850,24 @@ func (ac *AndalalinController) KeputusanHasil(ctx *gin.Context) {
 				go func() {
 					time.Sleep(1 * time.Minute)
 
-					var data models.Perlalin
+					var data2 models.Perlalin
 
-					result := ac.DB.First(&data, "id_andalalin = ?", id)
-					if result.Error != nil {
-						ctx.JSON(http.StatusBadGateway, gin.H{"status": "error", "message": result.Error})
+					result2 := ac.DB.First(&data2, "id_andalalin = ?", data.IdAndalalin)
+					if result2.Error != nil {
+						ctx.JSON(http.StatusBadGateway, gin.H{"status": "error", "message": result2.Error})
 						return
 					}
 
-					if data.StatusAndalalin == "Tunda pemasangan" {
-						ac.CloseTiketLevel1(ctx, data.IdAndalalin)
-						ac.BatalkanPermohonan(ctx, data)
-						data.Tindakan = "Permohonan dibatalkan"
-						data.PertimbanganTindakan = "Permohonan dibatalkan"
-						data.StatusAndalalin = "Permohonan dibatalkan"
-						ac.DB.Save(&data)
+					if data2.StatusAndalalin == "Tunda pemasangan" {
+						ac.CloseTiketLevel1(ctx, data2.IdAndalalin)
+						ac.BatalkanPermohonan(ctx, data2)
+						data2.Tindakan = "Permohonan dibatalkan"
+						data2.PertimbanganTindakan = "Permohonan dibatalkan"
+						data2.StatusAndalalin = "Permohonan dibatalkan"
+						ac.DB.Save(&data2)
 						updateChannel <- struct{}{}
 					}
 				}()
-
 			}
 		}()
 	} else if payload.Keputusan == "Batalkan permohonan" {
