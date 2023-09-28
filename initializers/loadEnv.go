a0b2297cf8db8f630fa19e141859a6bbc9f651fc
ikit777 <faisalmtp24@gmail.com>
2023-09-28 12:05:12 +0800

    commit

--- initializers/loadEnv.go
+++ initializers/loadEnv.go
@@ -34,7 +34,7 @@ type Config struct {
 
 func LoadConfig(path string) (config Config, err error) {
 	viper.AddConfigPath(path)
-	viper.SetConfigType("env")
+	viper.SetConfigName(".env")
 
 	viper.AutomaticEnv()
 
