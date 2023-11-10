e09b1d88a58f7cf51270a28d57f47d2dbae05341
ikit777 <faisalmtp24@gmail.com>
2023-11-11 02:52:20 +0800

    Initial commit

--- models/andalalin.model.go
+++ models/andalalin.model.go
@@ -690,8 +690,8 @@ type Administrasi struct {
 }
 
 type DataAdministrasi struct {
-	Persyaratan string  `json:"Persyaratan"`
-	Ada         *string `json:"Ada"`
-	Tidak       *string `json:"Tidak"`
-	Keterangan  *string `json:"Keterangan"`
+	Persyaratan string `json:"Persyaratan"`
+	Ada         string `json:"Ada"`
+	Tidak       string `json:"Tidak"`
+	Keterangan  string `json:"Keterangan"`
 }
