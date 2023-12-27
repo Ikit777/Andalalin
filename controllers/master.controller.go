44f421af26ab07ffb832dff8812e5606fcbcbf50
ikit777 <faisalmtp24@gmail.com>
2023-12-27 16:50:46 +0800

    Initial commit

--- controllers/master.controller.go
+++ controllers/master.controller.go
@@ -34,21 +34,45 @@ func NewDataMasterControler(DB *gorm.DB) DataMasterControler {
 func (dm *DataMasterControler) GetDataMaster(ctx *gin.Context) {
 	var master models.DataMaster
 
-	results_jenis := dm.DB.Select("jenis_proyek").First(&master)
+	results := dm.DB.First(&master)
 
-	if results_jenis.Error != nil {
-		ctx.JSON(http.StatusBadGateway, gin.H{"status": "error", "message": results_jenis.Error})
+	if results.Error != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"status": "error", "message": results.Error})
 		return
 	}
 
 	respone := struct {
-		IdDataMaster uuid.UUID `json:"id_data_master,omitempty"`
-		JenisProyek  []string  `json:"jenis_proyek,omitempty"`
-		UpdatedAt    string    `json:"update,omitempty"`
+		IdDataMaster               uuid.UUID                        `json:"id_data_master,omitempty"`
+		JenisProyek                []string                         `json:"jenis_proyek,omitempty"`
+		Lokasi                     []string                         `json:"lokasi_pengambilan,omitempty"`
+		KategoriRencanaPembangunan []string                         `json:"kategori_rencana,omitempty"`
+		JenisRencanaPembangunan    []models.JenisRencanaPembangunan `json:"jenis_rencana,omitempty"`
+		KategoriPerlengkapanUtama  []string                         `json:"kategori_utama,omitempty"`
+		KategoriPerlengkapan       []models.KategoriPerlengkapan    `json:"kategori_perlengkapan,omitempty"`
+		PerlengkapanLaluLintas     []models.JenisPerlengkapan       `json:"perlengkapan,omitempty"`
+		Persyaratan                models.Persyaratan               `json:"persyaratan,omitempty"`
+		Provinsi                   []models.Provinsi                `json:"provinsi,omitempty"`
+		Kabupaten                  []models.Kabupaten               `json:"kabupaten,omitempty"`
+		Kecamatan                  []models.Kecamatan               `json:"kecamatan,omitempty"`
+		Kelurahan                  []models.Kelurahan               `json:"kelurahan,omitempty"`
+		Jalan                      []models.Jalan                   `json:"jalan,omitempty"`
+		UpdatedAt                  string                           `json:"update,omitempty"`
 	}{
-		IdDataMaster: master.IdDataMaster,
-		JenisProyek:  master.JenisProyek,
-		UpdatedAt:    master.UpdatedAt,
+		IdDataMaster:               master.IdDataMaster,
+		JenisProyek:                master.JenisProyek,
+		Lokasi:                     master.LokasiPengambilan,
+		KategoriRencanaPembangunan: master.KategoriRencanaPembangunan,
+		JenisRencanaPembangunan:    master.JenisRencanaPembangunan,
+		KategoriPerlengkapanUtama:  master.KategoriPerlengkapanUtama,
+		KategoriPerlengkapan:       master.KategoriPerlengkapan,
+		PerlengkapanLaluLintas:     master.PerlengkapanLaluLintas,
+		Persyaratan:                master.Persyaratan,
+		Provinsi:                   master.Provinsi,
+		Kabupaten:                  master.Kabupaten,
+		Kecamatan:                  master.Kecamatan,
+		Kelurahan:                  master.Kelurahan,
+		Jalan:                      master.Jalan,
+		UpdatedAt:                  master.UpdatedAt,
 	}
 
 	ctx.JSON(http.StatusOK, gin.H{"status": "success", "data": respone})
